0830a88d817fccebf8956ce37912fa5d5f1de4fa
Gofy 2868 <pc@MacBookPro.fritz.box>
2026-07-19 11:49:42 -0700

    Zweites Projekt vorerst aus dem öffentlichen Repo entfernt

--- faith_impact_analysis/prayer_data_extended.csv.py
+++
@@ -1,8 +0,0 @@
-X = data[[
-    'Log GDP per capita', 'Social support',
-    'Healthy life expectancy at birth', 'Freedom to make life choices',
-    'Generosity', 'Perceptions of corruption',
-    'prayer_multiple_times_daily_percent',   # nur mehrmals täglich
-    'religious_service_weekly_percent',      # Gottesdienst
-    'religion_important_percent'             # subjektive Wichtigkeit
-]]
